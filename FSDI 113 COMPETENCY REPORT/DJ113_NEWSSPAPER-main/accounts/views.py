690abbb1bbcd084c53350af268e9f93f9e31fbda
ROBERT VALDES <69652470+ROBERTUABCMEX@users.noreply.github.com>
2021-01-03 20:25:35 -0800

    FSDI 113 COMPETENCY REPORT

---
+++ FSDI 113 COMPETENCY REPORT/DJ113_NEWSSPAPER-main/accounts/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from .forms import CustomUserCreationForm
+from django.views.generic import CreateView
+# , LoginView
+
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
+
+"""class LoginView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/login.html'
+"""
